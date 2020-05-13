138d8c12618823e0216b79a220bea801bb36012e
Matheus Dias <matheusmelhor3@outlook.com>
2020-05-13 10:45:58 -0300

    projeto feito algo mais

---
+++ src/entities/Worker.java
@@ -0,0 +1,91 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import entities.enums.WorkerLevel;
+
+public class Worker {
+	
+	private String name;
+	private WorkerLevel level;
+	private Double baseSalary;
+
+	private Departament departament;
+	private List<HourContract> contracts = new ArrayList<>();
+	
+	public Worker() {
+
+	}
+
+	public Worker(String name, WorkerLevel level, Double baseSalary, Departament departament) {
+		this.name = name;
+		this.level = level;
+		this.baseSalary = baseSalary;
+		this.departament = departament;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public WorkerLevel getLevel() {
+		return level;
+	}
+
+	public void setLevel(WorkerLevel level) {
+		this.level = level;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Departament getDepartament() {
+		return departament;
+	}
+
+	public void setDepartament(Departament departament) {
+		this.departament = departament;
+	}
+
+	public List<HourContract> getContracts() {
+		return contracts;
+	}
+
+
+//Adicionar e remover contratos da lista
+	public void addContract(HourContract contract) {
+		contracts.add(contract);
+	}
+	
+	public void removeContract(HourContract contract) {
+		contracts.remove(contract);
+	}
+	
+	public double income(int year, int month) {
+		double soma = baseSalary;
+		Calendar cal = Calendar.getInstance();
+		
+		for (HourContract x : contracts) {
+			cal.setTime(x.getDate());
+			int x_year = cal.get(Calendar.YEAR);
+			int x_month = 1 + cal.get(Calendar.MONTH);
+			if(year == x_year && month == x_month) {
+				soma += x.totalValue();
+			}
+			
+		}
+		return soma;
+	}
+	
+}
